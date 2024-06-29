5ee9a39b8c39758f0e5ee22707de1e51b81ad17c
Md. Sorwar Alam <esorwar.cse@gmail.com>
2024-06-29 22:15:13 +0600

    Add Test setup

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import unittest
-
+from unittest.mock import AsyncMock, patch
+import discord
+import os
 class TestSupperBot(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.token = os.getenv('DISCORD_TOKEN')
+        self.invalid_token = os.getenv('INVALID_TOKEN')
 
 
 
